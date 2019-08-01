b6bd79711829fdf0610e7f43363ed1fee10ab7a5
Graeme Rocher <graeme.rocher@gmail.com>
2019-08-01 12:50:17 +0200

    Add token

--- travis-build.sh
+++ travis-build.sh
@@ -72,27 +72,27 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         fi
 
 
-#        # If there is a tag present then this becomes the latest
-#        if [[ -n $TRAVIS_TAG ]]; then
-#            mkdir -p latest
-#            cp -r ../build/docs/. ./latest/
-#            git add latest/*
-#
-#            version="$TRAVIS_TAG"
-#            version=${version:1}
-#            majorVersion=${version:0:4}
-#            majorVersion="${majorVersion}x"
-#
-#            mkdir -p "$version"
-#            cp -r ../build/docs/. "./$version/"
-#            git add "$version/*"
-#
-#            mkdir -p "$majorVersion"
-#            cp -r ../build/docs/. "./$majorVersion/"
-#            git add "$majorVersion/*"
-#
-#        fi
-#
+       # If there is a tag present then this becomes the latest
+       if [[ -n $TRAVIS_TAG ]]; then
+           mkdir -p latest
+           cp -r ../build/docs/. ./latest/
+           git add latest/*
+
+           version="$TRAVIS_TAG"
+           version=${version:1}
+           majorVersion=${version:0:4}
+           majorVersion="${majorVersion}x"
+
+           mkdir -p "$version"
+           cp -r ../build/docs/. "./$version/"
+           git add "$version/*"
+
+           mkdir -p "$majorVersion"
+           cp -r ../build/docs/. "./$majorVersion/"
+           git add "$majorVersion/*"
+
+       fi
+
         git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
           git push origin HEAD || true
         }
