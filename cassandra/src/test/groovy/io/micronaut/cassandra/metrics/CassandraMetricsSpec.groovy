813a2668fcaaba2801d7ee0b475671cae9fc3a70
Dean Wette <wetted@unityfoundation.io>
2023-10-27 15:12:37 -0500

    Add support for using env vars to override cassandra datastax driver configuration properties.
    fixes #240

--- cassandra/src/test/groovy/io/micronaut/cassandra/metrics/CassandraMetricsSpec.groovy
+++ cassandra/src/test/groovy/io/micronaut/cassandra/metrics/CassandraMetricsSpec.groovy
@@ -1,15 +1,17 @@
 package io.micronaut.cassandra.metrics
 
 import com.datastax.oss.driver.api.core.CqlSession
+import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Meter
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 import io.micronaut.context.ApplicationContext
-import io.micronaut.context.DefaultApplicationContext
-import io.micronaut.context.env.MapPropertySource
 import io.micronaut.inject.qualifiers.Qualifiers
 import org.testcontainers.containers.CassandraContainer
 import org.testcontainers.utility.DockerImageName
+import spock.lang.Issue
 import spock.lang.Specification
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables
 
 class CassandraMetricsSpec extends Specification {
 
@@ -52,6 +54,50 @@ class CassandraMetricsSpec extends Specification {
         timers.size() == 2
 
         cleanup:
+        cassandra.stop()
+        applicationContext.close()
+    }
+
+    @Issue("https://github.com/micronaut-projects/micronaut-cassandra/issues/240")
+    void "test metrics with overriding env vars"() {
+        given:
+        // override: cassandra.default.basic.session-name=defaultSession
+        def env = new EnvironmentVariables("CASSANDRA_DEFAULT_BASIC_SESSION-NAME", "envSession")
+        env.setup()
+
+        CassandraContainer cassandra = new CassandraContainer(DockerImageName.parse('cassandra:latest'))
+        cassandra.start()
+
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'micronaut.metrics.enabled': true,
+                'cassandra.default.basic.contact-points': ["localhost:$cassandra.firstMappedPort"],
+                'cassandra.default.basic.session-name': 'defaultSession',
+                'cassandra.default.advanced.metrics.factory.class': 'MicrometerMetricsFactory',
+                'cassandra.default.advanced.metrics.session.enabled': ['connected-nodes', 'cql-requests'],
+                'cassandra.default.basic.load-balancing-policy.local-datacenter': 'datacenter1',
+                'test'
+        )
+
+        when:
+        CqlSession defaultCluster = applicationContext.getBean(CqlSession)
+        MeterRegistry meterRegistry = applicationContext.getBean(MeterRegistry)
+
+        then:
+        defaultCluster
+        meterRegistry
+
+        when:
+        Timer cqlRequests = meterRegistry.timer("envSession.cql-requests")
+        Counter bytesSent = meterRegistry.counter("envSession.bytes-sent")
+        Counter bytesReceived = meterRegistry.counter("envSession.bytes-received")
+
+        then:
+        cqlRequests
+        bytesSent
+        bytesReceived
+
+        cleanup:
+        env.teardown()
         cassandra.stop()
         applicationContext.close()
     }
