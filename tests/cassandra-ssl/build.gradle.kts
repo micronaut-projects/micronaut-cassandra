3f2a6c12ad16069c5f84a98ac823c92324cb47fd
Dean Wette <wetted@unityfoundation.io>
2024-01-19 11:44:16 -0600

    clean up builds

---
+++ tests/cassandra-ssl/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    groovy
+    id("io.micronaut.build.internal.cassandra-tests")
+}
+
+dependencies {
+    testImplementation(projects.micronautCassandra)
+    testImplementation(mn.micronaut.http.client)
+    testImplementation(mn.micronaut.jackson.databind)
+    testImplementation(mn.micronaut.management)
+    testImplementation(mnReactor.micronaut.reactor)
+
+    testImplementation(libs.bcpkix)
+    testImplementation(libs.testcontainers.spock)
+    testImplementation(libs.testcontainers.cassandra)
+
+    testRuntimeOnly(mnLogging.logback.classic)
+}
+
+micronaut {
+    version.set(libs.versions.micronaut.platform.get())
+    runtime("netty")
+    testRuntime("spock2")
+    processing {
+        incremental(true)
+        annotations("example.*")
+    }
+}
