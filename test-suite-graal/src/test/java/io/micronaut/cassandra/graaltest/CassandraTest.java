d7feba726cd7a2aa64815649399cf97ccda008f9
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-20 12:05:10 +0200

    disable in native image

--- test-suite-graal/src/test/java/io/micronaut/cassandra/graaltest/CassandraTest.java
+++ test-suite-graal/src/test/java/io/micronaut/cassandra/graaltest/CassandraTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.micronaut.context.annotation.Property;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 
@@ -12,6 +13,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 @Testcontainers(disabledWithoutDocker = true)
 class CassandraTest {
 
+    @DisabledInNativeImage
     @Test
     void testCassandra(CassandraRepository repository) {
         assertTrue(repository.getInfo().isPresent());
